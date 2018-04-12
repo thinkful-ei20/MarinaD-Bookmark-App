aa3d869a8dfd853403ba155247e029aa595ad1e3
Marina Dargitz <marina.dargitz@gmail.com>
2018-04-12 09:21:00 -0600

    Implement template string generationf or idividual bookmarks

--- scripts/bookmarks.js
+++ scripts/bookmarks.js
@@ -2,6 +2,29 @@
 'use strict';
 
 const BOOKMARKS = (function() {
+  
+  //Render Functions
+  const generateAdd = () => {
+
+  };
+
+  const generateExpanded = () => {
+
+  };
+
+  //generate template string based on bookmark object from local store
+  const generateBookmarks = (bookmark) => {
+    return `<div class="bookmark">
+    <button class="expandToggle">Expand</button>
+    <span class="bookmark-title">${bookmark.title}</span>
+    <span class="bookmark-ranking">${bookmark.rating}</span>
+  </div>`;
+  };
+
+  const render = () => {
+
+  };
+
   //Event Handlers
   const handleAdd = () => {
 
