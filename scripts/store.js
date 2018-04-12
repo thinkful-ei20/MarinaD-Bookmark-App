edfe01fa18eea0d005f72400d9073e0ba5a1a7e3
Marina Dargitz <marina.dargitz@gmail.com>
2018-04-11 18:13:43 -0600

    Create CRUD functions for store.js and test

--- scripts/store.js
+++ scripts/store.js
@@ -1,63 +1,84 @@
-/* global $ */
+/* global $ cuid*/
 'use strict';
 
 const STORE = (function() {
   
   //Local STORE varialbes, not public
-  const newBookmark = false;
-  const sortBy = 'all';
-  const expansionStatus = false;  
-  const bookmarks = [];
+  let newBookmark = false;
+  let sortBy = 'all';
+  let expansionStatus = false;  
+  let bookmarks = [
+    {
+      id : cuid(),
+      title: 'My test bookmark',
+      url: 'https://google.com',
+      rating: 4,
+      edit: false
+    },
+    {
+      id : 1,
+      title: 'My test bookmark',
+      url: 'https://google.com',
+      rating: 4,
+      edit: false
+    }
+  ];
 
   //CRUD Functions, public
 
   //Create Functions
   //create bookmark- newBookmark is an object containing all necessary keys
   const addBookmark = newBookmark => {
-
+    bookmarks.push(newBookmark);
   };
   
   //Read Functions
   const getNewBookmarkStatus = () => {
-
+    return newBookmark;
   };
 
   const getSortBy = () => {
-
+    return sortBy;
   };
 
   const getExpansionStatus = () => {
-
+    return expansionStatus;
   };
 
   const getBookmarkByID = (id) => {
-
+    return bookmarks.filter((bookmark)=> bookmark.id === id)[0];
   };
 
   //Update Functions
   //Set NewBookmark variable where status is true or false
   const setNewBookmark = (status) => {
-
+    newBookmark = status;
   };
 
   //Set SortBy variable where filter is a string
   const setSortBy = (filter) => {
-
+    sortBy = filter;
   };
 
   //Set ExpansionStatus where status is true or false
   const setExpansionStatus = (status) => {
-
+    expansionStatus = status;
   };
-  //Update a certain bookmark where bookmark is the object from the bookmarks array
-  const updateBookmark = (bookmark) => {
+  //Update a certain bookmark where newBookmark an object with some or all properties to edit
+  const updateBookmark = (newBookmark) => {
+    const bookmarkToEdit = getBookmarkByID(newBookmark.id);
+    Object.assign(bookmarkToEdit, newBookmark);
 
   };
 
   //Delete Functions
-  //Delete a certain bookmar where bookmark is the object from the bookmarks array
-  const deleteBookmark = (bookmark) => {
-
+  //Delete a certain bookmark where id is the id of the bookmark object from the bookmarks array
+  const deleteBookmark = (id) => {
+    console.log('delete bookmark ran');
+    const arrIndex = bookmarks.findIndex(bookmark => bookmark.id === id);
+    console.log(arrIndex);
+    bookmarks.splice(arrIndex, 1);
+    console.log(bookmarks);
   };
 
   return {
@@ -77,7 +98,13 @@ const STORE = (function() {
     updateBookmark,
 
     //delete functions
-    deleteBookmark
+    deleteBookmark,
+
+    //testing TO BE REMOVED
+    bookmarks,
+    newBookmark,
+    sortBy,
+    expansionStatus
   };
 }());
 
